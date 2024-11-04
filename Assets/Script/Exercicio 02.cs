c8e55ead738315a9a3a3f1706b3749f85f59bfda
PEDRO HENRIQUE MENESES VIEIRA DOS SANTOS <42587940826@info.local>
2024-11-04 15:14:43 -0300

    feat: Exercicio 02

--- Assets/Script/Exercicio 02.cs
+++ Assets/Script/Exercicio 02.cs
@@ -2,9 +2,33 @@ using UnityEngine;
 
 public class Exercicio02 : MonoBehaviour
 {
-     // 2° (Detecção de Power Up) Ao coletar  um power-up o personagem aumenta sua velocida. Se o jogador encontrar
+    // 2° (Detecção de Power Up) Ao coletar  um power-up o personagem aumenta sua velocida. Se o jogador encontrar
     // um  power-up, exiba "Power-Up Coletado".Caso contrario exiba "Nenhum Power Upencontrado".
 
+
+    [SerializeField] bool powerUp;
+   
+
+    void Start()
+    {
+        if (powerUp == true)
+        {
+            print("Power-up Coletado!");
+        }
+        else
+        {
+            print("Nenhum power-up encontrado");
+        }
+
+        //condição  Caso vdd : Caso falso 
+
+        print(powerUp ? "Power-up Coletado!" : "Nenhum power-up encontrado");
+
+    }
+    void Update()
+    {
+
+    }
 
 
 }

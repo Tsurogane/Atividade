4484afc45126ce77f1a1d563a01f994efc37202d
PEDRO HENRIQUE MENESES VIEIRA DOS SANTOS <42587940826@info.local>
2024-11-07 16:13:49 -0300

    Exercicio 11

--- Assets/Script/Exercicio 11.cs
+++ Assets/Script/Exercicio 11.cs
@@ -1,6 +1,44 @@
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
-public class Exercicio11
+public class Exercicio11 : MonoBehaviour
 {
-    
+
+    [SerializeField] int estado;
+
+    void Start()
+    {
+        switch (estado)
+        {
+            case 1:
+                print("Da para fazer com ternario?");
+                break;
+
+            case 2:
+                print("Sextooou!");
+                break;
+
+            case 3:
+                print("Cadê o Thiago?");
+                break;
+
+            case 4:
+                print("Cadê a Alana?");
+                break;
+
+            case 5:
+                print("Jucelino, cadê meu aumento?");
+                break;
+
+            default:
+                print("Não tenho nada a dizer sobre isso!");
+                break;
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
 }

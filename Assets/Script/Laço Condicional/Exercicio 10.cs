747d0e9bfc373852f0ac37b4bbbbfb32bf6bb9dd
PEDRO HENRIQUE MENESES VIEIRA DOS SANTOS <42587940826@info.local>
2024-11-13 14:29:23 -0300

    Ex01

--- Assets/Script/Laço Condicional/Exercicio 10.cs
+++ Assets/Script/Laço Condicional/Exercicio 10.cs
@@ -3,10 +3,7 @@ using static UnityEditor.Progress;
 
 public class Exercicio10 : MonoBehaviour
 {
-    //10. (Itens do inventário) Crie um script onde o jogador pode coletar
-    //diferentes tipos de itens(como moedas, poções, ou power-ups)
-    //que têm efeitos diferentes dependendo do tipo coletado.Use um
-    //switch case para determinar o efeito de cada tipo de item.
+    
 
     [SerializeField] string tipoItem;
     [SerializeField] int moeda = 10;
---
+++ Assets/Script/Laço Repetição/Ex01.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Ex01 : MonoBehaviour
+{ //1. (Contagem regressiva de tempo) Crie uma contagem
+    //regressiva de 5 a 1 e exiba "Início da partida!" ao final.
+    void Start()
+    {
+        //for(inicializador; condição; incremento/decremento) 
+        for (int i = 5; i > 0; i--)
+        {
+            print(i);
+        }
+        print("Início da partida!");
+    }
+
+
+    void Update()
+    {
+
+    }
+}
